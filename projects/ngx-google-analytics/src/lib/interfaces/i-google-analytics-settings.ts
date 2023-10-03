9c4d0f0450d5bed483d8c1071b26d02dd7e59607
Tomas Rimkus <sandshrew@gmail.com>
2023-10-03 11:05:12 +0200

    Update provider settings docs.

--- projects/ngx-google-analytics/src/lib/interfaces/i-google-analytics-settings.ts
+++ projects/ngx-google-analytics/src/lib/interfaces/i-google-analytics-settings.ts
@@ -4,16 +4,17 @@ import {IGoogleAnalyticsCommand} from './i-google-analytics-command';
  * Standardize a key-value object to configure GA installation.
  */
 export interface IGoogleAnalyticsSettings {
-    /** It's mandatory to provide a tracking code folks... */
+    /** GA4 Tag Id ("G-XXXXXX") */
     ga4TagId: string;
     /**
      * You can inject custom initialization commands like UserId or other e-commerce features.
-     * If set, it will run any GA Commands in sequence after setup GA environment.
+     * If set, it will run any GA Commands in sequence after setting up GA environment.
      */
     initCommands?: Array<IGoogleAnalyticsCommand>;
     /**
-     * If Google changes the uri and I die, you can survive!
-     * When set, it will change the default js script URI to the provided one.
+     * Instead of the default "https://www.googletagmanager.com/gtag/js" script, you can also use tag manager script:
+     * "https://www.googletagmanager.com/gtm.js". If you use "gtm.js", remember to also use tag manager ID:
+     * "GTM-XXXXXX" instead of gtag id "G-XXXXXX".
      */
     uri?: string;
     /** If true, trace GA tracking errors in production mode */
